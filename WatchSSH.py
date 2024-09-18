2abdb97e1a9a7f8c6c253ad64b7f9f6f7b80b6d0
Ryon Shane Hall <endorpheus@gmail.com>
2024-09-17 22:07:55 -0600

    added user ignore list and updated the about dialog

--- WatchSSH.py
+++ WatchSSH.py
@@ -1,13 +1,21 @@
+# todo:
+# track number of users sshing in/out
+# track number of ssh connections
+# add that number to the tooltip, if none, revert to "WatchSSH"
+
+
 import sys
 import argparse
 import os
 import subprocess
+import json
 from PySide6.QtCore import QThread, Signal, Qt, QPoint
 from PySide6.QtWidgets import (QApplication, QSystemTrayIcon, QMenu, QMessageBox, QDialog, 
-                               QVBoxLayout, QListWidget, QPushButton, QHBoxLayout, QLabel)
+                               QVBoxLayout, QListWidget, QPushButton, QHBoxLayout, QLabel,
+                               QInputDialog, QLineEdit)
 from PySide6.QtGui import QIcon, QAction, QMouseEvent
 
-VERSION = "v1.4"
+VERSION = "v1.5.0"
 
 def parse_args():
     parser = argparse.ArgumentParser(description="WatchSSH - An SSH Login/Logout Monitor")
@@ -19,10 +27,11 @@ def parse_args():
 class SSHActivityMonitor(QThread):
     activity_detected = Signal(str, str)  # Signal to emit username and action (login/logout)
 
-    def __init__(self, log_file):
+    def __init__(self, log_file, ignored_users):
         super().__init__()
         self.log_file = log_file
         self.running = True
+        self.ignored_users = ignored_users
 
     def run(self):
         try:
@@ -35,12 +44,14 @@ class SSHActivityMonitor(QThread):
                             parts = line.split()
                             if len(parts) > 8:
                                 username = parts[8]
-                                self.activity_detected.emit(username, "login")
+                                if username not in self.ignored_users:
+                                    self.activity_detected.emit(username, "login")
                         elif "session closed for user" in line:
                             parts = line.split()
                             if len(parts) > 5:
                                 username = parts[-1]
-                                self.activity_detected.emit(username, "logout")
+                                if username not in self.ignored_users:
+                                    self.activity_detected.emit(username, "logout")
                     else:
                         self.msleep(100)  # Sleep for 100ms if no new line
         except FileNotFoundError:
@@ -51,18 +62,18 @@ class SSHActivityMonitor(QThread):
     def stop(self):
         self.running = False
 
+    def update_ignored_users(self, ignored_users):
+        self.ignored_users = ignored_users
+
 class FramelessDialog(QDialog):
     def __init__(self, title, parent=None):
         super().__init__(parent)
         self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
-        # self.setAttribute(Qt.WA_TranslucentBackground)
-        
-        # # Set opacity to 95%
         self.setWindowOpacity(0.95)
 
         self.layout = QVBoxLayout(self)
         self.layout.setAlignment(Qt.AlignCenter)
-        self.layout.setContentsMargins(10, 0, 10, 5) # left, top, right, bottom?
+        self.layout.setContentsMargins(10, 0, 10, 5)
         
         self.title_bar = QLabel(title)
         self.title_bar.setAlignment(Qt.AlignCenter)
@@ -172,28 +183,94 @@ class AboutDialog(FramelessDialog):
         super().__init__("About WatchSSH", parent)
         
         content_layout = QVBoxLayout()
-        content_layout.setAlignment(Qt.AlignCenter)
+        #content_layout.setAlignment(Qt.AlignCenter)
         content_layout.addWidget(QLabel(f"WatchSSH {VERSION}", alignment=Qt.AlignCenter))
         content_layout.addWidget(QLabel("SSH Login/Logout Monitor", alignment=Qt.AlignCenter))
+        content_layout.addWidget(QLabel("by Ryon Shane Hall", alignment=Qt.AlignCenter))
+        content_layout.addWidget(QLabel("endorpheus@gmail.com", alignment=Qt.AlignCenter))
+        content_layout.addWidget(QLabel("Created: 202409180200", alignment=Qt.AlignCenter))
+        content_layout.addWidget(QLabel("An ounce of security equals a pound of paranoia.", alignment=Qt.AlignCenter))
         close_button = QPushButton("Close")
         close_button.clicked.connect(self.hide)
         
         content_layout.addWidget(close_button)
         self.layout.addLayout(content_layout)
 
+class IgnoredUsersDialog(FramelessDialog):
+    def __init__(self, ignored_users, parent=None):
+        super().__init__("Ignored Users", parent)
+        self.ignored_users = ignored_users
+
+        self.user_list = QListWidget()
+        self.layout.addWidget(self.user_list)
+
+        button_layout = QHBoxLayout()
+        self.add_button = QPushButton("Add User")
+        self.remove_button = QPushButton("Remove User")
+        self.close_button = QPushButton("Close")
+
+        button_layout.addWidget(self.add_button)
+        button_layout.addWidget(self.remove_button)
+        button_layout.addWidget(self.close_button)
+
+        self.layout.addLayout(button_layout)
+
+        self.add_button.clicked.connect(self.add_user)
+        self.remove_button.clicked.connect(self.remove_user)
+        self.close_button.clicked.connect(self.hide)
+
+        self.refresh_user_list()
+
+    def refresh_user_list(self):
+        self.user_list.clear()
+        for user in self.ignored_users:
+            self.user_list.addItem(user)
+
+    def add_user(self):
+        user, ok = QInputDialog.getText(self, "Add Ignored User", "Enter username:")
+        if ok and user:
+            if user not in self.ignored_users:
+                self.ignored_users.append(user)
+                self.refresh_user_list()
+
+    def remove_user(self):
+        selected_items = self.user_list.selectedItems()
+        if not selected_items:
+            QMessageBox.warning(self, "Warning", "Please select a user to remove.")
+            return
+
+        user = selected_items[0].text()
+        self.ignored_users.remove(user)
+        self.refresh_user_list()
+
 class LoginNotifier(QApplication):
     def __init__(self, args):
         super().__init__(sys.argv)
         self.args = args
+        self.ignored_users = self.load_ignored_users()
 
         if self.args.command_line_only:
             self.run_command_line_mode()
         else:
             self.setup_gui()
 
+    def load_ignored_users(self):
+        config_file = os.path.expanduser("~/.watchssh_config.json")
+        if os.path.exists(config_file):
+            with open(config_file, "r") as f:
+                config = json.load(f)
+                return config.get("ignored_users", [])
+        return []
+
+    def save_ignored_users(self):
+        config_file = os.path.expanduser("~/.watchssh_config.json")
+        config = {"ignored_users": self.ignored_users}
+        with open(config_file, "w") as f:
+            json.dump(config, f)
+
     def run_command_line_mode(self):
         print("Running in command-line mode. Press Ctrl+C to exit.")
-        self.monitor = SSHActivityMonitor(self.args.log_file)
+        self.monitor = SSHActivityMonitor(self.args.log_file, self.ignored_users)
         self.monitor.activity_detected.connect(self.print_activity)
         self.monitor.start()
 
@@ -210,10 +287,12 @@ class LoginNotifier(QApplication):
         # Create context menu
         self.menu = QMenu()
         self.close_action = QAction("Manage Connections", self)
+        self.ignored_users_action = QAction("Manage Ignored Users", self)
         self.about_action = QAction("About", self)
         self.quit_action = QAction("Quit", self)
 
         self.menu.addAction(self.close_action)
+        self.menu.addAction(self.ignored_users_action)
         self.menu.addAction(self.about_action)
         self.menu.addAction(self.quit_action)
 
@@ -221,11 +300,12 @@ class LoginNotifier(QApplication):
 
         # Connect signals
         self.close_action.triggered.connect(self.close_connection)
+        self.ignored_users_action.triggered.connect(self.manage_ignored_users)
         self.about_action.triggered.connect(self.show_about_dialog)
         self.quit_action.triggered.connect(self.quit)
 
         # Start monitoring
-        self.monitor = SSHActivityMonitor(self.args.log_file)
+        self.monitor = SSHActivityMonitor(self.args.log_file, self.ignored_users)
         self.monitor.activity_detected.connect(self.show_notification)
         self.monitor.start()
 
@@ -257,6 +337,13 @@ class LoginNotifier(QApplication):
     def close_connection(self):
         dialog = SSHConnectionDialog()
         dialog.exec()
+
+    def manage_ignored_users(self):
+        dialog = IgnoredUsersDialog(self.ignored_users)
+        dialog.exec()
+        self.ignored_users = dialog.ignored_users
+        self.save_ignored_users()
+        self.monitor.update_ignored_users(self.ignored_users)
 
     def show_about_dialog(self):
         dialog = AboutDialog()
